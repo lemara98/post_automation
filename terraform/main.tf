af0a22664b5c8556625e6dd8f882e7227da9c0d1
lemara98 <mile.knezevic98@gmail.com>
2025-12-03 23:16:46 +0100

    fix: change App Service Plan to F1 (Free) tier for Azure trial subscriptions

--- terraform/main.tf
+++ terraform/main.tf
@@ -126,13 +126,13 @@ resource "azurerm_application_insights" "main" {
   tags                = local.tags
 }
 
-# App Service Plan (Basic tier - more compatible with free/trial subscriptions)
+# App Service Plan (Free tier for Azure trial subscriptions)
 resource "azurerm_service_plan" "functions" {
   name                = "${local.resource_prefix}-plan"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   os_type             = "Linux"
-  sku_name            = "B1" # Basic tier (avoid consumption plan quota issues)
+  sku_name            = "F1" # Free tier (works with trial subscriptions)
   tags                = local.tags
 }
 
